f9900b1c4d57dbad1f959cffd1949c2dca293c29
Fábio M. Costa <fmc@ufg.br>
2026-05-06 10:24:30 -0300

    Update subscriber.py

--- subscriber.py
+++ subscriber.py
@@ -2,17 +2,17 @@ from kafka import KafkaConsumer
 from const import *
 import sys
 
-# Create consumer: Option 1 -- only consume new events
-consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-
-# Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
-#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
-
 try:
-  topic = sys.argv[1]
+    topic = sys.argv[1]
 except:
-  print ('Usage: python3 consumer <topic_name>')
-  exit(1)
+    print('Topic not provided as command-line argument, using default')
+    topic = TOPIC1
+
+# Create consumer: Option 1 -- only consumes new events (comment out Option 2 below)
+#consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+
+# Create consumer: Option 2 -- consumes old events (comment out Option 1 above)
+consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
   
 consumer.subscribe([topic])
 for msg in consumer:
