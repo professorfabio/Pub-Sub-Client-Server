d533a0edd25d71b8f6651d28d3e5d6a6b404c2ed
Fábio M. Costa <fmc@ufg.br>
2026-05-06 10:21:02 -0300

    Update subscriber-publisher.py

--- subscriber-publisher.py
+++ subscriber-publisher.py
@@ -3,15 +3,24 @@ from kafka import KafkaProducer
 from const import *
 import sys
 
+try:
+    topic1 = sys.argv[1]
+    topic2 = sys.argv[2]
+except:
+    print ('Topic 1 and Topic 2 not provided as command-line arguments, using default')
+    topic1 = TOPIC1
+    topic2 = TOPIC2
+
+# Consume old events (published before I started); switch the two lines below if this behavior is not desired
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
 #consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
 
-consumer.subscribe([TOPIC1])
+consumer.subscribe([topic1])
 for msg in consumer:
     print (msg.value)
-    txt = 'My message for ' + TOPIC2
-    producer.send(TOPIC2, value=txt.encode())
+    txt = 'My message for ' + topic2
+    producer.send(topic2, value=txt.encode())
     producer.flush()
